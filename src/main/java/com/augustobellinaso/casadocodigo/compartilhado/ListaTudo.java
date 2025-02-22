df106ef67e091fdf7d7a1a919e1584cb638a4a7e
Augusto Loose Bellinaso <augustobellinaso@gmail.com>
2025-02-21 21:38:17 -0300

    feat: endpoint para listar autores e categorias

---
+++ src/main/java/com/augustobellinaso/casadocodigo/compartilhado/ListaTudo.java
@@ -0,0 +1,29 @@
+package com.augustobellinaso.casadocodigo.compartilhado;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.List;
+
+@RestController
+public class ListaTudo {
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @GetMapping(value = "/lista-tudo")
+    public HashMap<String, Object> listar() {
+        HashMap<String, Object> resultado = new HashMap<>();
+        List<?> autores = entityManager.createQuery("SELECT a FROM Autor a").getResultList();
+
+        resultado.put("autores", autores.toString());
+
+        List<?> categorias = entityManager.createQuery("SELECT c FROM Categoria c").getResultList();
+        resultado.put("categorias", categorias.toString());
+
+        return resultado;
+    }
+}
